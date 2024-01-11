51ce90c74abe08410330e0a2123da8a301c61402
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-01-11 12:32:29 +0530

    added utils code

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+const asyncHandler = (requestHandler) =>{
+   return (req,res,next) => {
+        Promise.resolve(asyncHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export{
+    asyncHandler
+}
+
+// const asyncHandler = (fn) => async(err,req,res,next) => { // here i have used higher order function
+// try{
+//     await fn(err,req,res,next)
+// }catch(error){
+//     res.status(err.code || 500).json({
+//         success:false,
+//         message:err.message
+//     })
+// }
+// }
